de13ee3255519c70959d049525b3c0893b7d68a0
pixlark <t1000.pixlark@gmail.com>
2018-02-20 21:33:12 -0500

    refactor complete

--- src/main.cc
+++ src/main.cc
@@ -10,10 +10,6 @@
 #include "astar.h"
 #include "heap.h"
 
-#define REFACTOR 1
-
-#if REFACTOR
-
 Vector2i directions[] = {
 	{+0, -1}, // UP
 	{-1, +0}, // LEFT
@@ -30,6 +26,7 @@ struct Texture {
 enum EntityType {
 	ENTITY,
 	PLAYER,
+	GHOST,
 };
 
 struct Entity {
@@ -49,6 +46,11 @@ struct Player : Entity {
 	Vector2i queued_direction;
 };
 
+struct Ghost : Entity {
+	int power_type;
+	Vector2i direction;
+};
+
 struct Window {
 	Vector2i res;
 	SDL_Window * sdl;
@@ -61,6 +63,7 @@ static Window window;
 struct Level {
 	static const int play_w = 16;
 	static const int play_h = 13;
+	bool top_left = true;
 	int grid[play_w * play_h];
 	Vector2i crystal_pos;
 	List<Entity> walls;
@@ -160,6 +163,33 @@ void keydown_player(Player * p, SDL_Scancode scancode)
 	}
 }
 
+void make_ghost(Ghost * g, Vector2i pos, int power_type)
+{
+	Texture tex;
+	tex.pos = Vector2i(48 + (16 * power_type), 48);
+	tex.dim = Vector2i(16, 16);
+	tex.scale = Vector2f(1, 1);
+	make_entity(g, pos, tex);
+	g->power_type = power_type;
+	g->type = GHOST;
+}
+
+void update_ghost(Ghost * g, Player * player, Level * level)
+{
+	if (!g->moving) {
+		List<Vector2i> dirs = a_star(
+			level->grid, Level::play_w, Level::play_h,
+			g->grid_pos, player->grid_pos);
+		if (dirs.len > 0) {
+			g->direction = dirs[0];
+		} else {
+			g->direction = Vector2i(0, 0);
+		}
+		dirs.dealloc();
+	}
+	move_entity(g, g->grid_pos + g->direction);
+}
+
 Window make_window()
 {
 	float res_scale = 4.0;
@@ -186,7 +216,7 @@ bool v2_equality(Vector2i a, Vector2i b) {
 	return a.x == b.x && a.y == b.y;
 }
 
-void generate_level(Level * level, bool top_left)
+void generate_level(Level * level)
 {
 	auto add_walls_to_list = [level](Vector2i w, List<Vector2i> * l) {
 		for (int i = 0; i < 4; i++) {
@@ -223,7 +253,7 @@ void generate_level(Level * level, bool top_left)
 		walls.dealloc();
 	};
 	Vector2i start;
-	if (top_left) {
+	if (level->top_left) {
 		start.x = 1;
 		start.y = 1;
 		level->crystal_pos.x = Level::play_w - 2;
@@ -237,7 +267,7 @@ void generate_level(Level * level, bool top_left)
 	for (int i = 0; i < Level::play_w*Level::play_h; i++) level->grid[i] = 1;
 	tunnel_from_point(start);
 	{
-		Vector2i dir = top_left ? Vector2i(-1, -1) : Vector2i(1, 1);
+		Vector2i dir = level->top_left ? Vector2i(-1, -1) : Vector2i(1, 1);
 		while (level->grid[to_index(level->crystal_pos)]) {
 			// TODO(pixlark): This has about a 1/2048 chance (I
 			// think) of failing if the maze generation forms a
@@ -261,16 +291,43 @@ void generate_level(Level * level, bool top_left)
 			}
 		}
 	}
+	level->top_left = !level->top_left;
 }
 
-void draw_level(Level * level, int power)
+Vector2i get_empty_level_spot(Level * l)
 {
-	for (int i = 0; i < level->walls.len; i++) {
-		draw_entity(level->walls.arr + i);
+	Vector2i pos;
+	do {
+		pos.x = rand() % Level::play_w;
+		pos.y = rand() % Level::play_h;
+	} while (l->grid[to_index(pos)]);
+	return pos;
+}
+
+void draw_level(Level * l, int power)
+{
+	for (int i = 0; i < l->walls.len; i++) {
+		draw_entity(l->walls.arr + i);
 	}
 	Render::render(
-		Vector2i(level->crystal_pos.x * 16, level->crystal_pos.y * 16),
+		Vector2i(l->crystal_pos.x * 16, l->crystal_pos.y * 16),
 		Vector2i(power * 16 + 48, 0), Vector2i(16, 16), Vector2f(1, 1));
+}
+
+void update_level(Level * l, Player * player, Ghost * ghost)
+{
+	if (player->grid_pos.x == l->crystal_pos.x && player->grid_pos.y == l->crystal_pos.y) {
+		// Player stuff
+		// TODO(pixlark): Is it ok for this to be here?
+		player->power_max++;
+		player->power_level = player->power_max;
+		player->queued_direction = Vector2i(0, 0);
+		// Ghost stuff
+		// TODO(pixlark): Also don't think this should be here. Decouple somehow?
+		ghost->grid_pos = get_empty_level_spot(l);
+		// Level generation
+		generate_level(l);
+	}
 }
 
 void tick_delta_time()
@@ -292,9 +349,12 @@ int main()
 
 	Player player;
 	make_player(&player, Vector2i(1, 1), 0);
-
+	
 	Level level;
-	generate_level(&level, true);
+	generate_level(&level);
+
+	Ghost ghost;
+	make_ghost(&ghost, get_empty_level_spot(&level), 0);
 	
 	SDL_Event event;
 	bool running = true;
@@ -310,21 +370,25 @@ int main()
 			}
 		}
 		update_player(&player, &level);
+		update_ghost (&ghost,  &player, &level);
+		update_level (&level,  &player, &ghost);
 		Render::clear(RGBA(36, 56, 225, 255));
 		draw_level(&level, player.power_max);
 		draw_entity(&player);
+		draw_entity(&ghost);
+		
 		{
 			// Draw UI stuff
 			// TODO(pixlark): Put this all in a UI struct or something
 			for (int i = 0; i < 8; i++) {
 				if (i == player.power_level - 1) {
-					Render::render(Vector2i(i * 32, window.res.y - 32), Vector2i(0, 48), Vector2i(32, 32), Vector2i(1, 1));
+					Render::render(Vector2i(i * 32, window.res.y - 32), Vector2i(0, 48), Vector2i(32, 32), Vector2f(1, 1));
 				} else {
-					Render::render(Vector2i(i * 32, window.res.y - 32), Vector2i(0, 16), Vector2i(32, 32), Vector2i(1, 1));
+					Render::render(Vector2i(i * 32, window.res.y - 32), Vector2i(0, 16), Vector2i(32, 32), Vector2f(1, 1));
 				}
 			}
 			for (int i = 0; i < player.power_max; i++) {
-				Render::render(Vector2i(i * 32 + 8, window.res.y - 24), Vector2i(i * 16 + 48, 0), Vector2i(16, 16), Vector2i(1, 1));
+				Render::render(Vector2i(i * 32 + 8, window.res.y - 24), Vector2i(i * 16 + 48, 0), Vector2i(16, 16), Vector2f(1, 1));
 			}
 			Render::render(Vector2i(0, window.res.y - 48), Vector2i(0, 0), Vector2i(16, 16), Vector2f(16, 1));
 		}
@@ -333,341 +397,3 @@ int main()
 	}
 	return 0;
 }
-
-#else
-
-#define PLAY_W 16
-#define PLAY_H 13
-
-float delta_time = 0.0167;
-
-Vector2i directions[] = {
-	{+0, -1}, // UP
-	{-1, +0}, // LEFT
-	{+0, +1}, // DOWN
-	{+1, +0}, // RIGHT
-};
-
-// TODO(pixlark): Implement a binary search here or something
-// TODO(pixlark): Also make this a method on the Vector2
-bool contains(List<Vector2i> * l, Vector2i v)
-{
-	for (int i = 0; i < l->len; i++) {
-		if (l->arr[i].x == v.x && l->arr[i].y == v.y) return true;
-	}
-	return false;
-}
-
-enum Power {
-	NONE,
-	BLUE,
-	GREEN,
-};
-
-struct Sprite {
-	Vector2i pos;
-	Vector2i tex_pos;
-	Vector2i tex_size;
-	Vector2f scale;
-	void init(Vector2i pos, Vector2i tex_pos, Vector2i tex_size)
-	{
-		this->pos = pos;
-		this->tex_pos = tex_pos;
-		this->tex_size = tex_size;
-		this->scale = Vector2f(1, 1);
-	}
-	void render(Vector2i offset = {0, 0})
-	{
-		Render::render(pos, tex_pos + offset, tex_size, scale);
-	}
-};
-
-struct Level {
-	int level[PLAY_W * PLAY_H];
-	Vector2i crystal_pos;
-	void add_walls(Vector2i pos, List<Vector2i> * walls)
-	{
-		for (int i = 0; i < 4; i++) {
-			Vector2i n = pos + directions[i];
-			if (level[to_index(n)]) {
-				if (!contains(walls, n) &&
-					n.x >= 1 && n.x < PLAY_W-1 && n.y >= 1 && n.y < PLAY_H-1) {
-					walls->push(n);
-				}
-			}
-		}
-	}
-
-	void generate_level(bool top_left)
-	{
-		/* This doesn't quite work. I think we need to union-find on
-		   the crystal position and iterate tunneling until they
-		   connect.
-             -Paul T. Sun Feb 11 14:44:15 2018 */
-		auto tunnel_from_point = [this](int level[PLAY_W * PLAY_H], Vector2i p) {
-			List<Vector2i> walls;
-			level[to_index(p)] = 0;
-			walls.alloc();
-			add_walls(p, &walls);
-			while (1) {
-				if (walls.len == 0) break;
-				int wi = rand() % walls.len;
-				Vector2i w = walls[wi];
-				int bordering = 0;
-				for (int i = 0; i < 4; i++) {
-					if (!level[to_index(w + directions[i])]) {
-						bordering++;
-					}
-				}
-				if (bordering == 1) {
-					level[to_index(w)] = 0;
-					add_walls(w, &walls);
-				}
-				walls.remove(wi);
-			}
-			walls.dealloc();
-		};
-		Vector2i start;
-		if (top_left) {
-			start.x = 1;
-			start.y = 1;
-			crystal_pos.x = PLAY_W - 2;
-			crystal_pos.y = PLAY_H - 2;
-		} else {
-			start.x = PLAY_W - 2;
-			start.y = PLAY_H - 2;
-			crystal_pos.x = 1;
-			crystal_pos.y = 1;
-		}
-		for (int i = 0; i < PLAY_W*PLAY_H; i++) level[i] = 1;
-		tunnel_from_point(level, start);
-		{
-			Vector2i dir = top_left ? Vector2i(-1, -1) : Vector2i(1, 1);
-			while (level[to_index(crystal_pos)]) {
-				// TODO(pixlark): This has about a 1/2048 chance (I
-				// think) of failing if the maze generation forms a
-				// perfect diagonal line.
-				crystal_pos += dir;
-			}
-		}
-		printf("%d %d\n", crystal_pos);
-	}
-};
-
-struct Actor {
-	const float time_max = 0.5;
-	const float speed = 25;
-	Vector2f real_pos;
-	float time = 0;
-	Vector2i grid_pos;
-	Vector2i target_pos;
-	int power;
-	int selected_power;
-	Sprite sprite;
-	bool moving = false;
-	void init(Vector2i pos, Vector2i tex_pos, Vector2i tex_size)
-	{
-		power = NONE;
-		selected_power = power;
-		grid_pos = pos;
-		target_pos = grid_pos;
-		sprite.init(Vector2i(pos.x * 16, pos.y * 16), tex_pos, tex_size);
-		real_pos = Vector2f(pos.x * 16, pos.y * 16);
-	}
-	bool update(Level * level, Vector2i direction)
-	{
-		bool finished = false;
-		if (!moving) {
-			if (direction.x != 0 || direction.y != 0) {
-				moving = true;
-				time = time_max;
-			}
-		}
-		if (level->level[to_index(target_pos)]) {
-			target_pos = grid_pos + direction;
-		} else {
-			time += delta_time;
-			float t = time / time_max;
-			real_pos.x = (grid_pos.x * 16) + t * ((target_pos.x - grid_pos.x) * 16);
-			real_pos.y = (grid_pos.y * 16) + t * ((target_pos.y - grid_pos.y) * 16);
-			if (t >= 1) {
-				time = 0;
-				grid_pos = target_pos;
-				real_pos.x = grid_pos.x * 16;
-				real_pos.y = grid_pos.y * 16;
-				target_pos = grid_pos + direction;
-				finished = true;
-			}
-			sprite.pos = (Vector2i) real_pos;
-		}
-		return finished;
-	}
-};
-
-void level_to_walls(Level * level, List<Sprite> * walls) {
-	walls->dealloc();
-	walls->alloc();
-	for (int y = 0; y < PLAY_H; y++) {
-		for (int x = 0; x < PLAY_W; x++) {
-			if (level->level[to_index(x, y)]) {
-				Sprite sprite;
-				sprite.init(Vector2i(x * 16, y * 16), Vector2i(32, 0), Vector2i(16, 16));
-				walls->push(sprite);
-			}
-		}
-	}
-}
-
-int main()
-{
-	srand(time(NULL));
-	Vector2i base_res(256, 240);
-	float res_scale = 4.0;
-	
-	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Window * window = SDL_CreateWindow("NES game",
-		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-		base_res.x * res_scale, base_res.y * res_scale,
-		SDL_WINDOW_SHOWN|SDL_WINDOW_OPENGL);
-
-	{
-		List<char> exe_path = get_exe_dir();
-		exe_path.cat("..\\atlas.png", 13, 1);
-		printf("Loading atlas from %s\n", exe_path.arr);
-	
-		Render::init(window, exe_path.arr, base_res, res_scale);
-
-		exe_path.dealloc();
-	}
-
-	Vector2i start_pos = {1, 1};
-	
-	Level level;
-	bool top_left = true;
-	level.generate_level(top_left);
-
-	List<Sprite> walls;
-	level_to_walls(&level, &walls);
-
-	Actor player;
-	player.init(start_pos, Vector2i(32, 16), Vector2i(16, 16));
-	Vector2i queued_direction = Vector2i(0, 0);
-
-	Actor ghost;
-	{
-		Vector2i ghost_pos;
-		do {
-			ghost_pos.x = rand() % PLAY_W;
-			ghost_pos.y = rand() % PLAY_H;
-		} while (level.level[to_index(ghost_pos)]);
-		ghost.init(ghost_pos, Vector2i(48, 48), Vector2i(16, 16));
-	}
-	Vector2i ghost_direction = Vector2i(0, 0);
-	
-	Sprite barrier;
-	barrier.init(Vector2i(0, base_res.y - 48), Vector2i(0, 0), Vector2i(16, 16));
-	barrier.scale = Vector2f(16, 1);
-
-	uint32_t last_time = SDL_GetPerformanceCounter();
-	
-	SDL_Event event;
-	bool running = true;
-	while (running) {
-		while (SDL_PollEvent(&event) != 0) {
-			switch (event.type) {
-			case SDL_QUIT:
-				running = false;
-				break;
-			case SDL_KEYDOWN: {
-				switch (event.key.keysym.scancode) {
-				case SDL_SCANCODE_W: {
-					queued_direction = Vector2i(+0, -1);
-				} break;
-				case SDL_SCANCODE_A: {
-					queued_direction = Vector2i(-1, +0);
-				} break;
-				case SDL_SCANCODE_S: {
-					queued_direction = Vector2i(+0, +1);
-				} break;
-				case SDL_SCANCODE_D: {
-					queued_direction = Vector2i(+1, +0);
-				} break;
-				case SDL_SCANCODE_LEFT: {
-					if (player.selected_power > 1) {
-						player.selected_power--;
-					}
-				} break;
-				case SDL_SCANCODE_RIGHT: {
-					if (player.selected_power < player.power) {
-						player.selected_power++;
-					}
-				};
-				}
-			} break;
-			}
-		}
-		// Update
-		player.update(&level, queued_direction);
-		if (player.grid_pos.x == level.crystal_pos.x && player.grid_pos.y == level.crystal_pos.y) {
-			player.power++;
-			player.selected_power = player.power;
-			queued_direction = Vector2i(0, 0);
-			player.moving = false;
-			top_left = !top_left;
-			level.generate_level(top_left);
-			level_to_walls(&level, &walls);
-			{
-				Vector2i ghost_pos;
-				do {
-					ghost_pos.x = rand() % PLAY_W;
-					ghost_pos.y = rand() % PLAY_H;
-				} while (level.level[to_index(ghost_pos)]);
-				ghost.init(ghost_pos, Vector2i(48, 48), Vector2i(16, 16));
-			}
-		}
-		{
-			if (ghost.update(&level, directions[0])) {
-				List<Vector2i> directions = a_star(level.level, PLAY_W, PLAY_H, ghost.target_pos, player.grid_pos);
-				if (directions.len > 0) {
-					ghost_direction = directions[0];
-				}
-				directions.dealloc();
-			}
-		}
-		// Render
-		Render::clear(RGBA(36, 56, 225, 255));
-		for (int i = 0; i < walls.len; i++) {
-			walls[i].render();
-		}
-		Render::render(
-			Vector2i(level.crystal_pos.x * 16, level.crystal_pos.y * 16),
-			Vector2i(player.power * 16 + 48, 0), Vector2i(16, 16), Vector2f(1, 1));
-		ghost.sprite.render();
-		player.sprite.render(Vector2i(player.selected_power * 16, 0));
-		for (int i = 0; i < 8; i++) {
-			if (i == player.selected_power - 1) {
-				Render::render(Vector2i(i * 32, base_res.y - 32), Vector2i(0, 48), Vector2i(32, 32), Vector2i(1, 1));
-			} else {
-				Render::render(Vector2i(i * 32, base_res.y - 32), Vector2i(0, 16), Vector2i(32, 32), Vector2i(1, 1));
-			}
-		}
-		for (int i = 0; i < player.power; i++) {
-			Render::render(Vector2i(i * 32 + 8, base_res.y - 24), Vector2i(i * 16 + 48, 0), Vector2i(16, 16), Vector2i(1, 1));
-		}
-		barrier.render();
-		Render::swap(window);
-		// Manage framerate
-		{
-			uint32_t new_time = SDL_GetPerformanceCounter();
-			delta_time = (float) (new_time - last_time) / SDL_GetPerformanceFrequency();
-			last_time = new_time;
-		}
-		if (delta_time < 1.0 / 60.0) {
-			// Lock to 60
-			SDL_Delay(((1.0 / 60.0) - delta_time) * 1000);
-		}
-	}
-	
-	return 0;
-}
-#endif
